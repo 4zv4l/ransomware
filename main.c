491bc7f029dbb7ff7b0e5e757097a47f8cff8173
4zv4l <simon.blacks.be@gmail.com>
2021-09-21 17:23:57 +0200

    library setup

--- main.c
+++ main.c
@@ -2,17 +2,15 @@
     #define OS_Windows 0
     #include "src/liblin.h"
     #include <unistd.h>                 // linux kernel syscall
-    #include <stdlib.h>
-    #include <stdio.h>
-    #include <string.h>
 #elif defined(_WIN32) || defined(WIN32) // if Windows
     #define OS_Windows 1
     #include "src/libwin.h"
     #include <windows.h>                // Windows API
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
 #endif
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 
 int main (int argc, char *argv[]){
   printf("Here we go again...\n");
